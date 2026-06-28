ed5506d90f82032737507000f6b511e753dc8e3c
ourycodeur <kobeoury96@gmail.com>
2026-06-28 18:26:32 +0000

    Modification api

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,18 +10,6 @@ from api.main import app
 
 client = TestClient(app)
 
-def test_ask_endpoint():
-
-    response = client.post(
-        "/ask",
-        json={
-            "question":"concert à Paris"
-        }
-    )
-
-    assert response.status_code == 200
-
-    assert "answer" in response.json()
     
 def test_empty_question():
 
